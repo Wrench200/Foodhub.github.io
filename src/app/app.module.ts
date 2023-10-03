df636f1f19ca011b55a0f71d0f250d4b759de2ee
Wrench <tnjeik@gmail.com>
2023-10-03 17:53:56 +0100

    added transparent background to navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    require("flowbite/plugin"), // add this line
+  ],
+};
+
